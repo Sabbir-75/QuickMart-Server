f922c81b87c1ef6500cda4dc393e18667c86878b
Sabbir-75 <mdsabbirhossain9200@gmail.comm>
2025-08-24 07:02:18 +0600

    update credentials for env

--- index.js
+++ index.js
@@ -64,5 +64,5 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`QuickMart Database is Running on ${port}`);
+    console.log(`QuickMart Database Is Running On ${port}`);
 })
